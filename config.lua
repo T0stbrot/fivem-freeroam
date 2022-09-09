1ac89e6c0819827bd36748c6255e622ba3a6b75f
ye_ <51487454+ye01011001@users.noreply.github.com>
2022-09-09 20:50:01 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config						= {}
-Config.InfiniteAmmo		= true
-Config.InfiniteStamina  = true
-Config.NoWantedLevel    = true
-Config.PVP              = true
+Config.InfiniteAmmo		= true -- Infinite Ammo
+Config.InfiniteStamina  = true -- Infinite Stamina (You can run Infinitly)
+Config.NoWantedLevel    = true -- No Wanted Level
+Config.PVP              = true -- true = PVP On, false = PVP Off
+Config.KeepEngineOn     = true -- Keep The Engine from Vehicles On After Leaving
+Config.NoTraffic        = true -- Disable Traffic
